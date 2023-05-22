bc9bc5bdd895e0969b5167c936f33c30c34c62e7
AnnU <43593864+AnnOLoveCat@users.noreply.github.com>
2023-05-22 17:51:29 +0800

    上傳165資料

--- fingerlinebot/models.py
+++ fingerlinebot/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class Message(models.Model):
@@ -9,10 +10,8 @@ class Message(models.Model):
     line_id = models.CharField(max_length=50)
 
 class Ministry_Interior(models.Model):
-    name = models.CharField(max_length=50)
-    time = models.DateTimeField(auto_now_add=True)
-    url = models.URLField(max_length=200)
     line_id = models.CharField(max_length=50)
+    inform_date = models.DateField(default=date.today)
 
 class Mails(models.Model):
     mail = models.CharField(max_length=50)
