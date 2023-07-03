850a7da86bf60f6761f8dc44ce868f4f5d48f95d
AnnU <43593864+AnnOLoveCat@users.noreply.github.com>
2023-07-03 22:33:39 +0800

    修正使用者大小寫輸入無法查詢帳號的問題，新增新聞專區按鈕

--- fingerlinebot/admin.py
+++ fingerlinebot/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from fingerlinebot.models import *
 
 # Register your models here.
--- fingerlinebot/views.py
+++ fingerlinebot/views.py
@@ -7,9 +7,18 @@ from django.conf import settings
  
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextSendMessage
+from linebot.models import ( 
+    MessageEvent,
+    TextSendMessage,
+    TemplateSendMessage,
+    ButtonsTemplate,
+    MessageTemplateAction,
+    PostbackEvent,
+    PostbackTemplateAction,
+    PostbackAction,
+)
 
-from .models import Mails
+from .models import Mails,News
 
 import re
 import urllib.parse
@@ -38,27 +47,49 @@ def callback(request):
             if isinstance(event, MessageEvent):  # 如果有訊息事件
 
                 #使用者的訊息
-                msg = event.message.text
-
-                #資料庫裡的Mails資料，帳號跟被使用的是否
-                accounts = Mails.objects.filter(mail=msg)
-                
-                #回覆使用者訊息
+                user_msg = event.message.text.lower()
+                message = []
+                #篩選資料庫裡的Mails資料
+                accounts = Mails.objects.filter(mail=user_msg)
+                # #篩選資料庫資料庫裡的News資料
+                # news = News.objects.filter(news_title=)
+                #回覆給使用者訊息
                 content = ''
+
+                if user_msg == '更多功能':
+                    line_bot_api.reply_message(  # 回復傳入的訊息文字
+                        event.reply_token,
+                        TemplateSendMessage(
+                            alt_text='Buttons template',
+                            template=ButtonsTemplate(
+                                title='更多功能',
+                                text='請問要什麼功能?',
+                                actions=[
+                                    PostbackAction(
+                                        label='新聞情報',
+                                        text='新聞',
+                                        data='新聞'
+                                    ),
+                                ]
+                            )
+                        )
+                    )
+                
+                if user_msg == '新聞':
+                    message.append(TextSendMessage(text='測試成功'))
+
                 
                 for account in accounts:
-
-                    #判斷使用者輸入的帳號是否被使用過
-                    if msg == account.mail and account.used == '是':
-                        content += account.mail + '\n' +'此帳號有被使用過！！！'
+                    #判斷使用者輸入的帳號是否被使用過，回復傳入的訊息文字
+                    if user_msg == account.mail and account.used == '是':
+                        message.append(TextSendMessage(text= account.mail + '\n' +'此帳號有被使用過！！！'))
                     else:
-                        content += account.mail + '\n' +'此帳號無被使用過，請放心～'
-
+                        message.append(TextSendMessage(text=account.mail + '\n' +'此帳號無被使用過，請放心～'))
                 line_bot_api.reply_message(  # 回復傳入的訊息文字
                     event.reply_token,
-                    TextSendMessage(text=content),
+                    message,
                 )
-                print(content)
+                print(message)
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
